109cff272e5136f1a3b319c773bf320d27750585
skmayya1 <skandamayya4@gmail.com>
2024-12-18 14:20:55 +0530

    login route

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -2,9 +2,7 @@ import express from 'express';
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { createAndUpdateRoom } from './Utils/lib';
-
-
-
+import Prisma from './Utils/Prisma';
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: "*" } });
@@ -24,14 +22,31 @@ io.on("connection", (socket) => {
         socket.to(data.roomCode).emit("user-joined", socket.id);
         console.log("A user joined room", data.roomCode);
     });
-
-    
 });
 
 httpServer.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
+app.post('/signin', async (req, res) => {
+  const { solAddress } = req.body;
+  try {
+    const user = await Prisma.user.findUnique({
+      where: {
+        solAddress
+      }
+    });
+    if (user) res.send({ message: "User Exists , Login Succesfull", status: 200 });
+    const newUser = await Prisma.user.create({
+      data: {
+        solAddress
+      }
+    })
+    res.send({ message: "User Created", status: 200  });
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+    console.log(error);
+  }
+})
 //SOCKET CONNECTION EMITS :
 /*
   user-joined : When a user Joins a Room.
