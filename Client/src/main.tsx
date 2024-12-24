e3198c5ca2ff28a728526789eac40189580a79a9
skmayya1 <skandamayya4@gmail.com>
2024-12-24 12:06:27 +0530

    Completed

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { SocketProvider } from './Context/Socketcontext.tsx'
+import App from './App'
+import { SocketProvider } from './Context/Socketcontext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
